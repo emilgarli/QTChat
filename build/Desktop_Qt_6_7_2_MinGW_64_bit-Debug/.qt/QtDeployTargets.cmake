462427ff4afa7c51667b56cfabbf7f204f9aee72
Emil <emil.garli@gmail.com>
2024-09-30 23:57:27 +0200

    removed build folder from git

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ChattingApp_FILE D:/Source/repos/ChattingApp/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ChattingApp.exe)
-set(__QT_DEPLOY_TARGET_ChattingApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ChattingApp_RUNTIME_DLLS D:/Source/repos/Libs/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Source/repos/Libs/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Source/repos/Libs/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
